230f71c3019244f7f4fd027c2045a3f0cf27fbf9
Manju KC <Manju KC>
2019-05-08 18:18:14 -0400

    give some style to elements

--- friends/src/FriendCard.js
+++ friends/src/FriendCard.js
@@ -1,13 +1,13 @@
 import React from "react";
+import './friends.css';
 
 const FriendCard = ({ friends }) => {
     return (
-    <div>
-          <p>{friends.id}</p>
-        <p>{friends.name}</p>
-        <p>{friends.age}</p>
-        <p>{friends.email}</p>
-        <p> {friends.id}</p>
+    <div className = "friendscard">
+          <p>name:{friends.id}</p>
+        <p>Friendname:{friends.name}</p>
+        <p>Age:{friends.age}</p>
+        <p>Email:{friends.email}</p>
     </div>)
 }
 
--- friends/src/FriendsList.js
+++ friends/src/FriendsList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import FriendCard from "./FriendCard"
+import './friends.css';
 
 const FriendsList = props => {
     return (
-        <div>
+        <div className="friendlist">
             {props.friends.map((card, index) => {
                 return <FriendCard key={index} friends={card} />
             })}
